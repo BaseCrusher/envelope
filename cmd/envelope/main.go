169e57b0408cef05fba04edd04de24ba06691a05
BaseCrusher <basecrusher@users.noreply.github.com>
2026-08-06 20:12:48 +0200

    added out flag

--- cmd/envelope/main.go
+++ cmd/envelope/main.go
@@ -10,12 +10,19 @@ import (
 
 func main() {
 	prefix := flag.String("prefix", "", "only convert variables whose name starts with this prefix; the prefix is stripped from the key")
+	out := flag.String("out", "", "write YAML to this file instead of stdout")
 	flag.Parse()
 
-	out, err := envelope.Marshal(os.Environ(), *prefix)
+	doc, err := envelope.Marshal(os.Environ(), *prefix)
+	if err == nil {
+		if *out == "" {
+			_, err = os.Stdout.Write(doc)
+		} else {
+			err = os.WriteFile(*out, doc, 0o644)
+		}
+	}
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "envelope:", err)
 		os.Exit(1)
 	}
-	os.Stdout.Write(out)
 }
